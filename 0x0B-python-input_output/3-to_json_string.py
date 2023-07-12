fbad924ef239ffece032c4b71726e3ffc78fafe6
khadijamohamed99 <khadijamohamed5220@gmail.com>
2023-07-12 01:02:05 +0000

    inputs_outputs

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that convert object to JSON format"""
+import json
+
+
+def to_json_string(my_obj):
+    """Compute the JSON representation of an object (string)
+
+    Args:
+        my_obj (object): to convert
+    Returns:
+         the JSON representation of an object (string):
+    """
+
+    json_format = json.dumps(my_obj)
+    return json_format
